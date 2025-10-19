af4909c924f99bb2b3774e429fca2efb00c78d36
NastyaSianiuk <nastyamamayko@gmail.com>
2025-10-19 20:25:54 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask, jsonify
+import requests
+
+app = Flask(__name__)
+
+@app.route("/usd_to_byn")
+def usd_to_byn():
+    try:
+        url = "https://www.nbrb.by/api/exrates/rates/usd?parammode=2"
+        response = requests.get(url, timeout=5)
+        data = response.json()
+        return jsonify({"Cur_OfficialRate": data["Cur_OfficialRate"]})
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
